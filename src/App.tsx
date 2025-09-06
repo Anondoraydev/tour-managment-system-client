238131a7866d3d4c61a6e7b5516224acdd94731d
Anondoraydev <Anondo554@gmail.com>
2025-09-06 22:19:06 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+ 
+function App() { 
+
+  return (
+    <>
+       <h1>This is a simple react app with shadcn </h1>
+       <Button>Cleck Me</Button>
+    </>
+  )
+}
+
+export default App
