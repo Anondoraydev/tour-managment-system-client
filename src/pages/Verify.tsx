06fd872c278b93e9dcc34d1d43a11992749e4349
Anondoraydev <Anondo554@gmail.com>
2025-09-08 20:33:57 +0600

    email Verify added

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -1,20 +1,117 @@
-import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useLocation, useNavigate } from "react-router";
+import z from "zod";
+
+const FormSchema = z.object({
+  pin: z.string().min(6, {
+    message: "Your one-time password must be 6 characters.",
+  }),
+});
 
 export default function Verify() {
   const location = useLocation();
   const navigate = useNavigate();
   const [email] = useState(location.state);
 
-  useEffect(() => {
-    if (!email) {
-      navigate("/");
-    }
-  }, [email]);
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      pin: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data);
+    // navigate("/reset-password", { state: email });
+  };
+
+  //! need to fix this later
+  // useEffect(() => {
+  //   if (!email) {
+  //     navigate("/");
+  //   }
+  // }, [email]);
 
   return (
-    <div>
-      <h1> This is Verify component </h1>
+    <div className="grid place-content-center h-screen ">
+      <Card className="shadow-2xl rounded-2xl border border-gray-200 w-full max-w-md p-6">
+        <CardHeader className="mb-4">
+          <CardTitle className="text-2xl font-bold">
+            Verify your email address
+          </CardTitle>
+          <CardDescription className="text-gray-600 mt-1">
+            Please enter the 6-digit code we sent to <br />
+            {email}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              id="otp-form"
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-6">
+              <FormField
+                control={form.control}
+                name="pin"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>One-Time Password</FormLabel>
+                    <FormControl>
+                      <InputOTP maxLength={6} {...field}>
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                          <InputOTPSlot index={1} />
+                          <InputOTPSlot index={2} />
+                          <InputOTPSlot index={3} />
+                          <InputOTPSlot index={4} />
+                          <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+                    <FormDescription>
+                      Please enter the one-time password sent to your phone.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Button
+            form="otp-form"
+            type="submit"
+            className="bg-blue-600 hover:bg-blue-700 text-white">
+            Submit
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
